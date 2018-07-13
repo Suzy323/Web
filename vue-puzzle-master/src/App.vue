9fb671170b037ca69c2a926bd8a778f7547a5e3d
Suzy323 <838024325@qq.com>
2018-07-13 18:44:36 +0800

    hard is set ,time is ok, reset,go on, pause function is ok

--- vue-puzzle-master/src/App.vue
+++ vue-puzzle-master/src/App.vue
@@ -1,20 +1,26 @@
 <template>
-    <div class="box">
-        <div id="time">
-            <p id="mytime">时间 {{time}}</p>
-        </div>
-        <ul class="puzzle-wrap">
+<div class="box">
+    <div id="note">
+        <p id="myTime">时间 {{time}}</p>
+        <p id="myTime">步数 {{step}}</p>
+        <input type="text" v-model="tmp">
+        <button @click="setHard">难度系数确认</button>
+    </div>  
+    <div class="center">
+        <ul id="puzzle-wrap">
             <li 
                 :class="{'puzzle': true, 'puzzle-empty': !puzzle}" 
                 v-for="puzzle in puzzles" 
                 v-text="puzzle"
                 @click="moveFn($index)"
+                :style="'width:'+1/size*100+'%;height:'+1/size*100+'%;line-height:'+400/size+'px;'"
             ></li>
         </ul>
-        <button id="startreset" class="btn btn-warning btn-block btn-start" @click="start">开始游戏</button>
-        <button class="btn btn-warning btn-block btn-reset" @click="render">重置游戏</button>
-        <button class="btn btn-warning btn-block btn-reset" @click="render">暂停游戏</button>
+        <button id="start" class="btn btn-warning btn-block btn-start" @click="start" >开始游戏</button>
+        <button id="pause" class="btn btn-warning btn-block btn-start" @click="pause">暂停游戏</button>
+        <button id="continue" class="btn btn-warning btn-block btn-reset" @click="continue">继续游戏</button>
     </div>
+</div>
 </template>
 
 <script>
@@ -22,20 +28,62 @@ export default {
         
     data () {
         return {
-            time: "00:00:##",
+            time: "00:00",
             puzzles: [],
-            timer: null
+            timer: null,
+            n:0,
+            step:0,
+            size:4,
+            tmp:4
         }
     },
     methods: {
+        setHard(){
+            this.size=this.tmp;
+            this.setPuzzles();
+        },
+        
+        start(){
+            var oStart=document.getElementById("start");
+            oStart.innerHTML="重置游戏";
+            this.render ();
+            this.clock();
+        },
+        pause(){
+            clearInterval(this.timer);
+            var oDiv=document.getElementById("puzzle-wrap");
+            oDiv.style.visibility="hidden";
+            var oPause=document.getElementById("pause");
+            oPause.style.display="none";
+            var oContinue=document.getElementById("continue");
+            oContinue.style.display="inline";
+        },
+        continue(){
+            this.clock();
+            var oDiv=document.getElementById("puzzle-wrap");
+            oDiv.style.visibility="visible";
+            var oPause=document.getElementById("pause");
+            oPause.style.display="inline";
+            var oContinue=document.getElementById("continue");
+            oContinue.style.display="none";
+        },
+        setDisplay(obj,name,act){
+            var oBtn=document.getElementById(obj);
+            oBtn.style[name]=act;
+        },
 
         // 重置渲染
         render () {
+            clearInterval(this.timer);
+            this.time="00:00";
+            this.n=0;
+            this.step=0;
+            
             let puzzleArr = [],
                 i = 1
 
             // 生成包含1 ~ 15数字的数组
-            for (i; i < 16; i++) {
+            for (i; i < this.size*this.size; i++) {
                 puzzleArr.push(i)
             }
 
@@ -47,29 +95,23 @@ export default {
             // 页面显示
             this.puzzles = puzzleArr
             this.puzzles.push('')
-            this.time= "00:00:00"
         },
-        start(){
+        clock(){
             clearInterval(this.timer);
-            var n=0;
-            this.time="11:11";
             function toDub(n){
                 return {
                 n:n<10?("0"+n):(""+n)
                 }
             }   
-            var mmm=new toDub(11);
-            console.log(mmm);
-            setInterval(() => {
-                n++;
-                var m=parseInt(n/60)
-                var s=parseInt(n%60);
+            this.timer=setInterval(() => {
+                this.n++;
+                var m=parseInt(this.n/60)
+                var s=parseInt(this.n%60);
                 var mm=new toDub(m);
                 var ss=new toDub(s);
                 this.time=mm.n+":"+ss.n;
             },1000)
         },
-
         // 点击方块
         moveFn (index) {
 
@@ -77,22 +119,26 @@ export default {
             let curNum = this.puzzles[index],
                 leftNum = this.puzzles[index - 1],
                 rightNum = this.puzzles[index + 1],
-                topNum = this.puzzles[index - 4],
-                bottomNum = this.puzzles[index + 4]
+                topNum = this.puzzles[index - this.size],
+                bottomNum = this.puzzles[index + this.size]
 
             // 和为空的位置交换数值
-            if (leftNum === '' && index % 4) {
+            if (leftNum === '' && index % this.size) {
                 this.puzzles.$set(index - 1, curNum)
                 this.puzzles.$set(index, '')
-            } else if (rightNum === '' && 3 !== index % 4) {
+                this.step++;
+            } else if (rightNum === '' && (this.size-1) !== index % this.size) {
                 this.puzzles.$set(index + 1, curNum)
                 this.puzzles.$set(index, '')
+                this.step++;
             } else if (topNum === '') {
-                this.puzzles.$set(index - 4, curNum)
+                this.puzzles.$set(index - this.size, curNum)
                 this.puzzles.$set(index, '')
+                this.step++;
             } else if (bottomNum === '') {
-                this.puzzles.$set(index + 4, curNum)
+                this.puzzles.$set(index + this.size, curNum)
                 this.puzzles.$set(index, '')
+                this.step++;
             }
 
             this.passFn()
@@ -100,19 +146,30 @@ export default {
 
         // 校验是否过关
         passFn () {
-            if (this.puzzles[15] === '') {
-                const newPuzzles = this.puzzles.slice(0, 15)
+            if (this.puzzles[this.size*this.size-1] === '') {
+                var newPuzzles = this.puzzles.slice(0, this.size*this.size-1)
 
-                const isPass = newPuzzles.every((e, i) => e === i + 1)
+                var isPass = newPuzzles.every((e, i) => e === i + 1)
 
                 if (isPass) {
                     alert ('恭喜，闯关成功！')
                 }
             }
+        },
+        setPuzzles(){
+            let puzzleArr = [],
+                    i = 1
+
+            // 生成包含1 ~ 15数字的数组
+            for (i; i < this.size*this.size; i++) {
+                puzzleArr.push(i)
+            }
+            this.puzzles = puzzleArr
+            this.puzzles.push('')
         }
     },
     ready () {
-        this.render()
+        this.setPuzzles()
     }
 }
 </script>
@@ -126,10 +183,20 @@ body {
 
 .box {
     width: 400px;
-    margin: 50px auto 0;
+    margin: 30px auto 0;
+    height:100%;
 }
-
-.puzzle-wrap {
+.center{
+    position:relative;
+    top:20px;
+    background:#ccc;
+}
+#note {
+    position:relative;
+    border:1px solide black;
+    top:50px;
+}
+#puzzle-wrap {
     width: 400px;
     height: 400px;
     margin-bottom: 40px;
@@ -140,12 +207,9 @@ body {
 
 .puzzle {
     float: left;
-    width: 100px;
-    height: 100px;
     font-size: 20px;
     background: #f90;
     text-align: center;
-    line-height: 100px;
     border: 1px solid #ccc;
     box-shadow: 1px 1px 4px;
     text-shadow: 1px 1px 1px #B9B4B4;
@@ -162,13 +226,27 @@ body {
 }
 .btn-reset {
     box-shadow: inset 2px 2px 18px;
+    display:none;
 }
-#mytime{
-    margin-left: 300px;
-    width: 100px;
+
+#note input{
+    width: 50px;
+    height: 20px;
+    right:120px;
+    position: absolute;
+}
+#note button{
+    width: 120px;
+    height: 20px;
+    right:0px;
+    position: absolute;
+}
+#note p{
+    margin-left: 0px;
+    width: 80px;
     height: 50px;
-    text-align: center;
-    line-height:50px;
     border:none;
+    display: inline-block;
+
 }
 </style>
